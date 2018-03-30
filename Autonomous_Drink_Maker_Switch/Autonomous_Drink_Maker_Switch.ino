c97f51a2f4a9819c64a963fc9935a60298fa468d
seandoughty <sean.doughty@outlook.com>
2018-03-30 18:13:43 -0400

    Saturday work

--- Autonomous_Drink_Maker_Switch/Autonomous_Drink_Maker_Switch.ino
+++ Autonomous_Drink_Maker_Switch/Autonomous_Drink_Maker_Switch.ino
@@ -1,6 +1,20 @@
-int buttonPushCounter[4] = {0,0,0,0};   // counter for the number of button presses
-int buttonState[4] = {0,0,0,0};         // current state of the button
-int lastButtonState[4] = {0,0,0,0};     // previous state of the button
+int buttonPushCounter[5] = {0,0,0,0,0};   // counter for the number of button presses
+int buttonState[5] = {0,0,0,0,0};         // current state of the button
+int lastButtonState[5] = {0,0,0,0,0};     // previous state of the button
+//Declare pin functions on Arduino
+#define stp 48
+#define dir 3
+#define MS1 52
+#define MS2 5
+#define MS3 6
+#define EN  50
+
+//Declare variables for functions
+char user_input;
+int x;
+int y;
+int state;
+int tnt = 0;
 
 void setup() {
   pinMode(22, OUTPUT);
@@ -14,7 +28,7 @@ void setup() {
   pinMode(38, OUTPUT);
   pinMode(40, OUTPUT);
   pinMode(42, OUTPUT);
-  pinMode(44, OUTPUT);
+  pinMode(47, OUTPUT);
   
   pinMode(A0, INPUT);
   pinMode(A1, INPUT);
@@ -22,6 +36,13 @@ void setup() {
   pinMode(A3, INPUT);
   pinMode(A4, INPUT);
 
+  pinMode(stp, OUTPUT);
+  pinMode(dir, OUTPUT);
+  pinMode(MS1, OUTPUT);
+  pinMode(MS2, OUTPUT);
+  pinMode(MS3, OUTPUT);
+  pinMode(EN, OUTPUT);
+  resetBEDPins(); //Set step, direction, microstep and enable pins to default states
   Serial.begin(9600);
   
 }
@@ -30,7 +51,8 @@ void loop() {
   buttonState[1] = analogRead(A1);
   buttonState[2] = analogRead(A2);
   buttonState[3] = analogRead(A3);
-  
+  buttonState[4] = analogRead(A4);
+
   // compare the buttonState to its previous state
   if (buttonState[0] != lastButtonState[0]) {
     // if the state has changed, increment the counter
@@ -43,9 +65,6 @@ void loop() {
       Serial.print("number of button pushes:  ");
       Serial.println(buttonPushCounter[0]);     
     } 
-    else {
-      delay(100);
-    }
   }
 
   // compare the buttonState to its previous state
@@ -56,13 +75,10 @@ void loop() {
       // if the current state is HIGH then the button
       // went from off to on:
       buttonPushCounter[1]++;
-      delay(250);
+      delay(100);
       Serial.print("number of button pushes:  ");
       Serial.println(buttonPushCounter[1]);     
     } 
-    else {
-      delay(100);
-    }
   }
 
   if (buttonState[2] != lastButtonState[2]) {
@@ -76,15 +92,11 @@ void loop() {
       Serial.print("number of button pushes:  ");
       Serial.println(buttonPushCounter[2]);     
     } 
-    else {
-      delay(100);
-    }
   }
 
   if (buttonState[3] != lastButtonState[3]) {
     // if the state has changed, increment the counter
  
-    if (buttonState[3] == 0) {
       // if the current state is HIGH then the button
       // went from off to on:
       buttonPushCounter[3]++;
@@ -92,14 +104,20 @@ void loop() {
       Serial.print("number of button pushes:  ");
       Serial.println(buttonPushCounter[3]);     
     } 
-    else {
-      delay(100);
-    }
   }
 
-
-
-
+  if (buttonState[4] != lastButtonState[4]) {
+    // if the state has changed, increment the counter
+ 
+    if (buttonState[4] == 0) {
+      // if the current state is HIGH then the button
+      // went from off to on:
+      buttonPushCounter[4]++;
+      delay(250);
+      Serial.print("number of button pushes:  ");
+      Serial.println(buttonPushCounter[4]);     
+    } 
+  }
 
   
   // save the current state as the last state, 
@@ -108,6 +126,7 @@ void loop() {
   lastButtonState[1] = buttonState[1];
   lastButtonState[2] = buttonState[2];
   lastButtonState[3] = buttonState[3];
+  lastButtonState[4] = buttonState[4];
 
   
   if (buttonPushCounter[0]%4 > 0){
@@ -116,57 +135,58 @@ void loop() {
   }
   if (buttonPushCounter[0]%4 > 1){
     //Serial.println("on");
-    digitalWrite(24, HIGH);
+    digitalWrite(26, HIGH);
   }
   if (buttonPushCounter[0]%4 > 2){
     //Serial.println("on");
-    digitalWrite(26, HIGH);
+    digitalWrite(34, HIGH);
   }  
   else if (buttonPushCounter[0]%4 == 0){
     digitalWrite(22,LOW); 
-    digitalWrite(24,LOW);
-    digitalWrite(26,LOW); 
+    digitalWrite(26,LOW);
+    digitalWrite(34,LOW); 
   }
 
   if (buttonPushCounter[1]%4 > 0){
     //Serial.println("on");
-    digitalWrite(28, HIGH);
+    digitalWrite(24, HIGH);
   }
   if (buttonPushCounter[1]%4 > 1){
     //Serial.println("on");
-    digitalWrite(30, HIGH);
+    digitalWrite(28, HIGH);
   }
   if (buttonPushCounter[1]%4 > 2){
     //Serial.println("on");
-    digitalWrite(32, HIGH);
+    digitalWrite(36, HIGH);
   }  
   else if (buttonPushCounter[1]%4 == 0){
-    digitalWrite(28,LOW); 
-    digitalWrite(30,LOW);
-    digitalWrite(32,LOW); 
+    digitalWrite(24,LOW); 
+    digitalWrite(28,LOW);
+    digitalWrite(36,LOW); 
   }
 
   if (buttonPushCounter[2]%4 > 0){
-    //Serial.println("on");
-    digitalWrite(34, HIGH);
+    Serial.println("on34");
+    digitalWrite(30, HIGH);
   }
   if (buttonPushCounter[2]%4 > 1){
     //Serial.println("on");
-    digitalWrite(36, HIGH);
+    digitalWrite(47, HIGH);
   }
   if (buttonPushCounter[2]%4 > 2){
     //Serial.println("on");
     digitalWrite(38, HIGH);
   }  
-  else if (buttonPushCounter[0]%4 == 0){
-    digitalWrite(34,LOW); 
-    digitalWrite(36,LOW);
+  else if (buttonPushCounter[2]%4 == 0){
+    //Serial.println("off34");
+    digitalWrite(30,LOW); 
+    digitalWrite(47,LOW);
     digitalWrite(38,LOW); 
   }
 
   if (buttonPushCounter[3]%4 > 0){
     //Serial.println("on");
-    digitalWrite(40, HIGH);
+    digitalWrite(32, HIGH);
   }
   if (buttonPushCounter[3]%4 > 1){
     //Serial.println("on");
@@ -174,12 +194,159 @@ void loop() {
   }
   if (buttonPushCounter[3]%4 > 2){
     //Serial.println("on");
-    digitalWrite(44, HIGH);
+    digitalWrite(40, HIGH);
   }  
-  else if (buttonPushCounter[0]%4 == 0){
-    digitalWrite(40,LOW); 
+  else if (buttonPushCounter[3]%4 == 0){
+    digitalWrite(32,LOW); 
     digitalWrite(42,LOW);
-    digitalWrite(44,LOW); 
+    digitalWrite(40,LOW); 
+  }
+
+  if (buttonPushCounter[4] == 1){
+    Serial.println("make_drink");
+
+        
+    //SmallStepMode();
+    ReverseStep();
+
+
+    buttonPushCounter[0] = 0;
+    buttonPushCounter[1] = 0;
+    buttonPushCounter[2] = 0;
+    buttonPushCounter[3] = 0;
+    buttonPushCounter[4] = 0;
+    buttonPushCounter[5] = 0;
   }
 }
 
+
+//Reset Big Easy Driver pins to default states
+void resetBEDPins()
+{
+  digitalWrite(stp, LOW);
+  digitalWrite(dir, LOW);
+  digitalWrite(MS1, LOW);
+  digitalWrite(MS2, LOW);
+  digitalWrite(MS3, LOW);
+  digitalWrite(EN, HIGH);
+}
+
+//Default microstep mode function
+void StepForwardDefault()
+{
+  Serial.println("Moving forward at default step mode.");
+  digitalWrite(dir, LOW); //Pull direction pin low to move "forward"
+  for(x= 1; x<500; x++)  //Loop the forward stepping enough times for motion to be visible
+  {
+    digitalWrite(stp,HIGH); //Trigger one step forward
+    delay(1);
+    digitalWrite(stp,LOW); //Pull step pin low so it can be triggered again
+    delay(1);
+  }
+  Serial.println("Enter new option");
+  Serial.println();
+}
+
+//Default microstep mode function
+void StepForward()
+{
+  Serial.println("Moving forward at default step mode.");
+  digitalWrite(dir, LOW); //Pull direction pin low to move "forward"
+  for(x= 1; x<2700; x++)  //Loop the forward stepping enough times for motion to be visible
+  {
+    digitalWrite(stp,HIGH); //Trigger one step forward
+    delay(1);
+    digitalWrite(stp,LOW); //Pull step pin low so it can be triggered again
+    delay(1);
+  }
+  Serial.println("Enter new option");
+  Serial.println();
+}
+
+
+void ReverseStep()
+{
+  Serial.println("Moving in reverse at default step mode.");
+  digitalWrite(dir, HIGH); //Pull direction pin high to move in "reverse"
+  for(x= 1; x<2700; x++)  //Loop the stepping enough times for motion to be visible
+  {
+    digitalWrite(stp,HIGH); //Trigger one step
+    delay(1);
+    digitalWrite(stp,LOW); //Pull step pin low so it can be triggered again
+    delay(1);
+  }
+  Serial.println("Enter new option");
+  Serial.println();
+}
+
+
+
+//Reverse default microstep mode function
+void ReverseStepDefault()
+{
+  Serial.println("Moving in reverse at default step mode.");
+  digitalWrite(dir, HIGH); //Pull direction pin high to move in "reverse"
+  for(x= 1; x<500; x++)  //Loop the stepping enough times for motion to be visible
+  {
+
+    if (tnt == 1)
+      {
+         break;
+      }
+  
+    digitalWrite(stp,HIGH); //Trigger one step
+    delay(1);
+    digitalWrite(stp,LOW); //Pull step pin low so it can be triggered again
+    delay(1);
+  }
+  Serial.println("Enter new option");
+  Serial.println();
+}
+
+// 1/16th microstep foward mode function
+void SmallStepMode()
+{
+  Serial.println("Stepping at 1/16th microstep mode.");
+  digitalWrite(dir, LOW); //Pull direction pin low to move "forward"
+  digitalWrite(MS1, HIGH); //Pull MS1,MS2, and MS3 high to set logic to 1/16th microstep resolution
+  digitalWrite(MS2, HIGH);
+  digitalWrite(MS3, HIGH);
+  for(x= 1; x<1000; x++)  //Loop the forward stepping enough times for motion to be visible
+  {
+    digitalWrite(stp,HIGH); //Trigger one step forward
+    delay(1);
+    digitalWrite(stp,LOW); //Pull step pin low so it can be triggered again
+    delay(1);
+  }
+  Serial.println("Enter new option");
+  Serial.println();
+}
+
+//Forward/reverse stepping function
+void ForwardBackwardStep()
+{
+  Serial.println("Alternate between stepping forward and reverse.");
+  for(x= 1; x<5; x++)  //Loop the forward stepping enough times for motion to be visible
+  {
+    //Read direction pin state and change it
+    state=digitalRead(dir);
+    if(state == HIGH)
+    {
+      digitalWrite(dir, LOW);
+    }
+    else if(state ==LOW)
+    {
+      digitalWrite(dir,HIGH);
+    }
+    
+    for(y=1; y<1000; y++)
+    {
+      digitalWrite(stp,HIGH); //Trigger one step
+      delay(1);
+      digitalWrite(stp,LOW); //Pull step pin low so it can be triggered again
+      delay(1);
+    }
+  }
+  Serial.println("Enter new option");
+  Serial.println();
+}
